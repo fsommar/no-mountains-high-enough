d4a5f6ddc965c60000316200ef2e48ca40de2494
Fredrik Sommar <fsommar@kth.se>
2014-09-08 16:32:45 +0200

    Initialized eclipse project

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export LC_COLLATE=C
+
+/info/adk14/labb1/tokenizer < /info/adk14/labb1/korpus | sort > /var/tmp/K
---
+++ src/csc/kth/adk14/Main.java
@@ -0,0 +1,12 @@
+package csc.kth.adk14;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Test");
+	}
+
+}
